35e1ae689773c870f700950b30e8dd30ca8779ee
K.Kazu <64839248+kazu09@users.noreply.github.com>
2023-09-16 19:52:21 +0900

    add zxing read qrcode visible url

--- app/src/main/java/com/kazu/qrcodesample/util/CameraPreviewUtils.kt
+++ app/src/main/java/com/kazu/qrcodesample/util/CameraPreviewUtils.kt
@@ -3,11 +3,13 @@ package com.kazu.qrcodesample.util
 import android.content.Context
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.zxing.MultiFormatReader
 import java.util.concurrent.Executors
 
 class CameraPreviewUtils(
@@ -24,6 +26,11 @@ class CameraPreviewUtils(
     /** lifecycleOwner */
     private var lifecycleOwner: LifecycleOwner
 
+    enum class ReadType( val readType: Int) {
+        ZXING(0),
+        ML_KIT(1)
+    }
+
     init {
         previewView = _previewView
         context = _context
@@ -33,7 +40,7 @@ class CameraPreviewUtils(
     /**
      * start camera preview
      */
-    fun startCamera() {
+    fun startCamera(callback: ReadQrCallBack,readType: Int) {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
@@ -42,17 +49,25 @@ class CameraPreviewUtils(
             val cameraSelector =
                 CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
+            val multiFormatReader = MultiFormatReader()
+
             val imageAnalysis = ImageAnalysis.Builder().build()
             imageAnalysis.setAnalyzer(
                 Executors.newSingleThreadExecutor(),
                 ImageAnalysis.Analyzer { image ->
-                    image.close()
+                    callback.zxing(image, multiFormatReader,readType)
+                    callback.mlKit(image, readType)
+
                 })
 
-            cameraProvider.unbindAll()
             cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis)
             preview.setSurfaceProvider(previewView.surfaceProvider)
 
         }, ContextCompat.getMainExecutor(context))
+    }
+
+    interface ReadQrCallBack {
+        fun zxing(image: ImageProxy, multiFormatReader: MultiFormatReader, readType: Int)
+        fun mlKit(image: ImageProxy, readType: Int)
     }
 }
---
+++ app/src/main/java/com/kazu/qrcodesample/viewmodel/ZxingViewModel.kt
@@ -0,0 +1,48 @@
+package com.kazu.qrcodesample.viewmodel
+
+import android.os.Handler
+import android.os.Looper
+import androidx.camera.core.ImageProxy
+import androidx.lifecycle.ViewModel
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.RGBLuminanceSource
+import com.google.zxing.common.HybridBinarizer
+
+class ZxingViewModel:ViewModel() {
+    var callback: QrReadText? = null
+
+    fun qrCodeAnalyzer(image: ImageProxy, multiFormatReader: MultiFormatReader) {
+        val buffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        val width = image.width
+        val height = image.height
+        val source = RGBLuminanceSource(width, height, bytes.toIntArray())
+        val bitmap = BinaryBitmap(HybridBinarizer(source))
+        try {
+            val result = multiFormatReader.decode(bitmap)
+            // update visible lauout
+            Handler(Looper.getMainLooper()).post {
+                callback?.readSuccess(result.text)
+            }
+        } catch (e: Exception) {
+            // qr not found or exception
+            Handler(Looper.getMainLooper()).post {
+                // update gone lauout
+                callback?.readFailure()
+            }
+        } finally {
+            image.close()
+        }
+    }
+
+    interface QrReadText {
+        fun readSuccess(url: String)
+        fun readFailure()
+    }
+
+    private fun ByteArray.toIntArray(): IntArray {
+        return this.map { it.toInt() }.toIntArray()
+    }
+}
